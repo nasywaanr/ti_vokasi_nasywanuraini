4cdd6b670affa4036b8ba3c104f964a66bc63344
nasywaanr <nasywanuraini38@gmail.com>
2025-03-14 19:58:35 +0700

    fetch: add iot internet

---
+++ iot_internet/src/main.cpp
@@ -0,0 +1,58 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+// Ganti dengan kredensial jaringan Wi-Fi Anda
+// const char* ssid = "Lab IT";
+// const char* password = "labit2024";
+
+ const char* ssid = "Wokwi-GUEST";
+ const char* password = "";
+
+// URL lengkap server yang akan diakses
+const char* serverUrl = "http://0326-140-213-52-84.ngrok-free.app";
+
+// Interval waktu antara setiap permintaan (dalam milidetik)
+const unsigned long interval = 5000;
+unsigned long previousMillis = 0;
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+
+  Serial.print("Menghubungkan ke WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" Terhubung!");
+}
+
+void loop() {
+
+  unsigned long currentMillis = millis();
+
+  // Periksa apakah interval waktu telah berlalu
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    if (WiFi.status() == WL_CONNECTED) {
+      HTTPClient http;
+
+      // Inisialisasi HTTPClient dengan URL server
+      http.begin(serverUrl);
+
+      // Mengirim permintaan HTTP GET
+      int httpResponseCode = http.GET();
+
+      // Menampilkan kode status HTTP
+      Serial.print("Kode status HTTP: ");
+      Serial.println(httpResponseCode);
+
+      // Menutup koneksi
+      http.end();
+    } else {
+      Serial.println("WiFi tidak terhubung.");
+    }
+  }
+}
